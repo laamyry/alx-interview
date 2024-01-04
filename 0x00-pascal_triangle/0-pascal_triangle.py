aff63ac82aef40fc252123d25cee2c4277f0a582
laamyry <slamiri10@gmail.com>
2024-01-04 22:22:08 +0100

    add task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Pascal's Triangle'''
+def pascal_triangle(n):
+
+    result = []
+    if n > 0:
+
+        for m in range(1, n+1):
+            lev = []
+            j = 1
+            for k in range (1, m + 1):
+                lev.append(j)
+                j = j * (m - k) // k
+            result.append(lev)
+    return result
