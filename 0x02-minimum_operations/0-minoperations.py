0964e3391d3e02d1b60d05c77222bb6fc2df9baa
laamyry <slamiri10@gmail.com>
2024-01-18 11:24:09 +0100

    add task

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+def minOperations(n):
+    pasted_chars = 1
+    clipboard = 0
+    counter = 0
+
+    while pasted_chars < n:
+        if clipboard == 0:
+            clipboard = pasted_chars
+            counter += 1
+
+        if pasted_chars == 1:
+            pasted_chars += clipboard
+            counter += 1
+            continue
+
+        remaining = n - pasted_chars
+        if remaining < clipboard:
+            return 0
+
+        if remaining % pasted_chars != 0:
+            pasted_chars += clipboard
+            counter += 1
+        else:
+            clipboard = pasted_chars
+            pasted_chars += clipboard
+            counter += 2
+
+    if pasted_chars == n:
+        return counter
+    else:
+        return 0
