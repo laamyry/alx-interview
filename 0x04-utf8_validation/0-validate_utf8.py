e3243b0d44f1ab91db40782edb5540833c424042
laamyry <slamiri10@gmail.com>
2024-02-01 20:51:58 +0100

    add task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def validUTF8(data):
+    '''validUTF8'''
+    number_bytes = 0
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+        mask_byte = 1 << 7
+        if number_bytes == 0:
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+            if number_bytes == 0:
+                continue
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
+        number_bytes -= 1
+
+    return number_bytes == 0
