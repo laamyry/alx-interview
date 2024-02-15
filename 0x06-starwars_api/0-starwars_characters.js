43f689261a5a0841f7341f82f90d202f461d5fa7
laamyry <slamiri10@gmail.com>
2024-02-15 14:29:08 +0100

    update task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 
 request(url, async (err, response, body) => {
