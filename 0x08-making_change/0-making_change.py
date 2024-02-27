1f2be30811808f258b9765d52f29f56ffc826fd9
laamyry <slamiri10@gmail.com>
2024-02-27 16:35:36 +0100

    add task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,15 @@
+def makeChange(coins, total):
+    """makeChange"""
+    if not coins or coins is None:
+        return -1
+    if total <= 0:
+        return 0
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        for _ in range(total // coin):
+            total -= coin
+            change += 1
+        if total == 0:
+            return change
+    return -1
